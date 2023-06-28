6baa614f150a126a52a768904887051e63a096df
Joberth Rogers <joberth.rogers18@gmail.com>
2023-06-28 00:51:38 -0300

    feat: add initial files api

---
+++ ApiSteelDoor/src/main/java/com/steelDoor/SteelDoorApplication.java
@@ -0,0 +1,13 @@
+package com.steelDoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteelDoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteelDoorApplication.class, args);
+	}
+
+}
---
+++ ApiSteelDoor/src/test/java/com/steelDoor/SteelDoorApplicationTests.java
@@ -0,0 +1,8 @@
+package com.steelDoor;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteelDoorApplicationTests {
+
+}
