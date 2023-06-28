1f9b9e15ad719d61ebd31bcb302d23c0618d8073
Joberth Rogers <joberth.rogers18@gmail.com>
2023-06-28 02:13:17 -0300

    feat: add dockerfile to api

--- ApiSteelDoor/src/main/java/com/steelDoor/SteelDoorApplication.java
+++ ApiSteelDoor/src/main/java/com/steelDoor/SteelDoorApplication.java
@@ -2,9 +2,17 @@ package com.steelDoor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SteelDoorApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SteelDoorApplication.class, args);
