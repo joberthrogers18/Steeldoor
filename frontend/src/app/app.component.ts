ab32eacadcb75115938ea492ae1324c1ca32e115
Joberth Rogers <joberth.rogers18@gmail.com>
2023-07-02 10:27:44 -0300

    feat: setup primeng styles framework

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  items: MenuItem[] | undefined = [];
 }
