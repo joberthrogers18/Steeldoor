068958a924fd984e900b9feeaf1e1f74c0458e3d
Joberth Rogers <joberth.rogers18@gmail.com>
2023-07-02 23:43:28 -0300

    feat: add filter side

--- frontend/src/app/job/job-list/job-list.component.ts
+++ frontend/src/app/job/job-list/job-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-job-list',
   templateUrl: './job-list.component.html',
-  styleUrls: ['./job-list.component.scss']
+  styleUrls: ['./job-list.component.scss'],
 })
-export class JobListComponent {
+export class JobListComponent implements OnInit {
+  formGroup!: FormGroup;
 
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      title: new FormControl(''),
+      desSalary: new FormControl(''),
+    });
+  }
 }
--- frontend/src/app/job/job.module.ts
+++ frontend/src/app/job/job.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { JobRoutingModule } from './job-routing.module';
 import { JobListComponent } from './job-list/job-list.component';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [JobListComponent],
-  imports: [CommonModule, JobRoutingModule, ButtonModule],
+  imports: [
+    CommonModule,
+    JobRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+  ],
 })
 export class JobModule {}
